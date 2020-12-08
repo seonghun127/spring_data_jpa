d757b515015620636209147c9b14ded68980cb48
seonghun127 <seonghun127@gmail.com>
2020-12-08 21:16:31 +0900

    first commit

---
+++ src/test/java/com/inflearn/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
