17af01dd937f34de73c787ec12a34ac1b65b7814
seonghun127 <seonghun127@gmail.com>
2020-12-13 18:34:00 +0900

    MemberRepository 추가

--- src/main/java/com/inflearn/springdatajpa/domain/member/MemberRepository.java
+++ src/main/java/com/inflearn/springdatajpa/domain/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.inflearn.springdatajpa.domain.member;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,16 @@ public class MemberRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", name)
+                .getResultList();
     }
 }
