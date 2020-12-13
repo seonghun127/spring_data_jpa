4f6dc10e987ad020d0485e0d612e71bfa6939763
seonghun127 <seonghun127@gmail.com>
2020-12-13 18:28:47 +0900

    CASCADE 설정

--- src/main/java/com/inflearn/springdatajpa/domain/order/Order.java
+++ src/main/java/com/inflearn/springdatajpa/domain/order/Order.java
@@ -7,6 +7,7 @@ import com.inflearn.springdatajpa.orderitem.OrderItem;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -38,10 +39,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
