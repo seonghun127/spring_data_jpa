ba2c2d67f4c1667bcd3941b9db88e85ce0c342d8
seonghun127 <seonghun127@gmail.com>
2020-12-13 16:31:47 +0900

    OneToOne의 fetch 전략을 LAZY로 변경

--- src/main/java/com/inflearn/springdatajpa/domain/delivery/Delivery.java
+++ src/main/java/com/inflearn/springdatajpa/domain/delivery/Delivery.java
@@ -8,6 +8,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
