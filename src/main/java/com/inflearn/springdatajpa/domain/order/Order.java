b6ded8599aca87fc1733ca8c24212c337955fd43
seonghun127 <seonghun127@gmail.com>
2020-12-15 21:32:06 +0900

    Order, OrderItem 엔티티에 비지니스 로직 추가

--- src/main/java/com/inflearn/springdatajpa/domain/order/Order.java
+++ src/main/java/com/inflearn/springdatajpa/domain/order/Order.java
@@ -1,6 +1,7 @@
 package com.inflearn.springdatajpa.domain.order;
 
 import com.inflearn.springdatajpa.domain.delivery.Delivery;
+import com.inflearn.springdatajpa.domain.delivery.vo.DeliveryStatus;
 import com.inflearn.springdatajpa.domain.member.Member;
 import com.inflearn.springdatajpa.domain.order.vo.OrderStatus;
 import com.inflearn.springdatajpa.domain.orderitem.OrderItem;
@@ -50,4 +51,48 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 }
