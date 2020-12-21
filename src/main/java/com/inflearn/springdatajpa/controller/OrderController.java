a4b6b85330d3d3867bdbab4599f2b7d03a82126e
seonghun127 <seonghun127@gmail.com>
2020-12-21 21:15:05 +0900

    주문 검색 및 취소 기능 추가

--- src/main/java/com/inflearn/springdatajpa/controller/OrderController.java
+++ src/main/java/com/inflearn/springdatajpa/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.inflearn.springdatajpa.controller;
 
 import com.inflearn.springdatajpa.domain.item.Item;
 import com.inflearn.springdatajpa.domain.member.Member;
+import com.inflearn.springdatajpa.domain.order.Order;
+import com.inflearn.springdatajpa.domain.order.dto.OrderSearch;
 import com.inflearn.springdatajpa.service.ItemService;
 import com.inflearn.springdatajpa.service.MemberService;
 import com.inflearn.springdatajpa.service.OrderService;
@@ -9,6 +11,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -40,6 +44,20 @@ public class OrderController {
     public String order(@RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count) {
         orderService.order(memberId, itemId, count);
 
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findAll(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
