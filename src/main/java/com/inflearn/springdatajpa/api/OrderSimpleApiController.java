493d662c57b98c44bf8742760579ff6eb7347e19
seonghun127 <seonghun127@gmail.com>
2021-01-18 23:17:09 +0900

    OrderRepository에 fetch join으로 조회하는 findAllWithMemberDelivery() 메서드 추가

--- src/main/java/com/inflearn/springdatajpa/api/OrderSimpleApiController.java
+++ src/main/java/com/inflearn/springdatajpa/api/OrderSimpleApiController.java
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * xToOne (ManyToOne, OneToOne)
- *  Order
- *  Order -> Member
- *  Order -> Delivery
+ * Order
+ * Order -> Member
+ * Order -> Delivery
  */
 @RestController
 public class OrderSimpleApiController {
@@ -30,7 +30,7 @@ public class OrderSimpleApiController {
 
     /**
      * API에서 엔티티를 반환화는 상황
-     *
+     * <p>
      * 엔티티 자체를 API에서 반환할 때 양방향의 연관관계가 있다면 순환 참조가 발생하여
      * Serialize를 실패하고 stack overflow가 발생하여 에러가 난다.
      * -> 임시적으로 이 문제를 해결하려면 엔티티 레벨에서 jackson 라이브러리가
@@ -52,6 +52,13 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         return orderRepository.findAll(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        return orderRepository.findAllWithMemberDelivery().stream()
                 .map(SimpleOrderDto::new)
                 .collect(Collectors.toList());
     }
